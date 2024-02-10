68cca083d7cd5d13794eade75089cb9e9ecd4565
= <=>
2024-02-10 22:43:08 +0545

    changes

--- lib/booked.dart
+++ lib/booked.dart
@@ -1,84 +1,123 @@
 import 'package:flutter/material.dart';
 
 class Booked extends StatelessWidget {
-  const Booked({super.key});
+  const Booked({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     return Container(
-          padding: const EdgeInsets.all(10),
-          margin: EdgeInsets.all(10.0),
-          height: 130,
-          width: screenWidth * 0.9,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.white,
-          ),
-          child: Wrap(
-              children: [
-              FractionallySizedBox(
-              widthFactor: 0.3,
-              child: Container(
-                height: 110,
-                decoration: BoxDecoration(
-                    borderRadius:
-                    BorderRadius.circular(10),
-                    image: DecorationImage(
-                        image: AssetImage(
-                            'assets/hotel4.jpeg'),
-                        fit: BoxFit.cover)
-                ),
-              )
-          ),
-       Column(
+      child: Column(
         children: [
-          FractionallySizedBox(
-            widthFactor: 0.35,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          Container(
+            padding: const EdgeInsets.all(10),
+            margin: EdgeInsets.all(10.0),
+            height: 130,
+            width: screenWidth * 0.9,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+            ),
+            child: Wrap(
               children: [
-                Text(
-                  'President Hotel',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.location_on_outlined,
-                      size: 17,
-                      color: Colors.grey.withOpacity(0.9),
+                FractionallySizedBox(
+                  widthFactor: 0.3,
+                  child: Container(
+                    height: 110,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: AssetImage('assets/hotel4.jpeg'),
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                    Text(
-                      'Paris, France',
-                      style: TextStyle(
-                          color: Colors.grey
-                              .withOpacity(0.9)),
-                    ),
-                  ],
+                  ),
                 ),
-                SizedBox(height: 15),
-                Row(
-                  children: [
-                    Text(
-                      '\$30',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
+                SizedBox(width: 15),
+                FractionallySizedBox(
+                  widthFactor: 0.35,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'President Hotel',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.location_on_outlined,
+                            size: 17,
+                            color: Colors.grey.withOpacity(0.9),
+                          ),
+                          Text(
+                            'Paris, France',
+                            style: TextStyle(
+                              color: Colors.grey.withOpacity(0.9),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 15),
+                      Row(
+                        children: [
+                          Text(
+                            '\$30',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '/per night',
+                            style: TextStyle(
+                              color: Colors.grey.withOpacity(0.9),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                FractionallySizedBox(
+                  widthFactor: 0.3,
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                            ),
+                            Text(
+                              '4.9',
+                              style: TextStyle(
+                                color: Colors.pink,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          '(4,599 reviews)',
+                          style: TextStyle(
+                            color: Colors.grey.withOpacity(0.9), fontSize: 12
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Icon(Icons.bookmark, color: Colors.pink, size: 30,)
+                      ],
                     ),
-                    Text(
-                      '/per night',
-                      style: TextStyle(
-                          color: Colors.grey
-                              .withOpacity(0.9)),
-                    )
-                  ],
+                  ),
                 ),
               ],
             ),
@@ -100,30 +139,28 @@ class Booked extends StatelessWidget {
                   child: Container(
                     height: 110,
                     decoration: BoxDecoration(
-                        borderRadius:
-                        BorderRadius.circular(10),
-                        image: DecorationImage(
-                            image: AssetImage(
-                                'assets/hotel3.jpeg'),
-                            fit: BoxFit.cover)),
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: AssetImage('assets/hotel3.jpeg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
                 ),
                 SizedBox(width: 15),
                 FractionallySizedBox(
                   widthFactor: 0.35,
                   child: Column(
-                    crossAxisAlignment:
-                    CrossAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         'Hayatt Hotel',
                         style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 17),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                        ),
                       ),
-                      SizedBox(
-                        height: 5,
-                      ),
+                      SizedBox(height: 5),
                       Row(
                         children: [
                           Icon(
@@ -134,8 +171,8 @@ class Booked extends StatelessWidget {
                           Text(
                             'Paris, France',
                             style: TextStyle(
-                                color: Colors.grey
-                                    .withOpacity(0.9)),
+                              color: Colors.grey.withOpacity(0.9),
+                            ),
                           ),
                         ],
                       ),
@@ -145,19 +182,55 @@ class Booked extends StatelessWidget {
                           Text(
                             '\$32',
                             style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                           Text(
                             '/per night',
                             style: TextStyle(
-                                color: Colors.grey
-                                    .withOpacity(0.9)
+                              color: Colors.grey.withOpacity(0.9),
                             ),
                           )
                         ],
-                      )
+                      ),
                     ],
+                  ),
+                ),
+                FractionallySizedBox(
+                  widthFactor: 0.3,
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                            ),
+                            Text(
+                              '4.7',
+                              style: TextStyle(
+                                color: Colors.pink,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          '(4,479 reviews)',
+                          style: TextStyle(
+                            color: Colors.grey.withOpacity(0.9), fontSize: 12
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Icon(Icons.bookmark, color: Colors.pink, size: 30,)
+                      ],
+                    ),
                   ),
                 ),
               ],
@@ -165,7 +238,6 @@ class Booked extends StatelessWidget {
           ),
         ],
       ),
-    ])
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -26,179 +26,179 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            backgroundColor: Colors.pink.shade50,
-            resizeToAvoidBottomInset: true,
-            extendBody: true,
-            body: Center(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-                child: FractionallySizedBox(
-                  widthFactor: 0.95,
-                  heightFactor: 0.95,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white60,
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(30),
-                        bottom: Radius.circular(30),
-                      ),
+          backgroundColor: Colors.pink.shade50,
+          resizeToAvoidBottomInset: true,
+          extendBody: true,
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              child: FractionallySizedBox(
+                widthFactor: 0.95,
+                heightFactor: 0.95,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white60,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(30),
+                      bottom: Radius.circular(30),
                     ),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Expanded(
-                                  child: ListTile(
-                                    leading: CircleAvatar(
-                                      backgroundImage:
-                                          AssetImage('assets/picture.jpg'),
+                  ),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: ListTile(
+                                  leading: CircleAvatar(
+                                    backgroundImage:
+                                    AssetImage('assets/picture.jpg'),
+                                  ),
+                                  title: Text(
+                                    'Hello',
+                                    style: TextStyle(
+                                      color: Colors.grey.withOpacity(0.9),
+                                      fontSize: 10,
                                     ),
-                                    title: Text(
-                                      'Hello',
-                                      style: TextStyle(
-                                        color: Colors.grey.withOpacity(0.9),
-                                        fontSize: 10,
-                                      ),
-                                    ),
-                                    subtitle: Text(
-                                      'Helia Dulia!',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 15,
-                                      ),
+                                  ),
+                                  subtitle: Text(
+                                    'Helia Dulia!',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
                                     ),
                                   ),
                                 ),
-                                Container(
-                                  height: 35,
-                                  width: 45,
-                                  decoration: BoxDecoration(
-                                    color: Colors.redAccent,
-                                    borderRadius: BorderRadius.circular(20),
+                              ),
+                              Container(
+                                height: 35,
+                                width: 45,
+                                decoration: BoxDecoration(
+                                  color: Colors.redAccent,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Center(
+                                  child: SvgPicture.asset(
+                                    'assets/bell.svg',
+                                    height: 15,
+                                    width: 20,
+                                    color: Colors.white,
                                   ),
-                                  child: Center(
-                                    child: SvgPicture.asset(
-                                      'assets/bell.svg',
-                                      height: 15,
-                                      width: 20,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
+                                ),
+                              )
+                            ],
                           ),
+                        ),
 
-                          Search(),
+                        Search(),
 
-                          SizedBox(height: 15),
-                          SingleChildScrollView(
+                        SizedBox(height: 15),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: [
+                              SizedBox(width: 10),
+                              buildIconContainer(
+                                  'assets/hotel.svg', 'Hotel', Colors.red),
+                              SizedBox(width: 10),
+                              buildIconContainer('assets/flight.svg',
+                                  'Flight', Colors.green),
+                              SizedBox(width: 10),
+                              buildIconContainer('assets/location.svg',
+                                  'Location', Colors.purple.shade300),
+                              SizedBox(width: 10),
+                              buildIconContainer('assets/food.svg', 'Food',
+                                  Colors.blueAccent.shade700),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 25),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Popular Hotels',
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                'See All',
+                                style: TextStyle(
+                                    color: Colors.pink, fontSize: 13),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Container(
+                          height: 190,
+                          child: ListView(
                             scrollDirection: Axis.horizontal,
-                            child: Row(
-                              children: [
-                                SizedBox(width: 10),
-                                buildIconContainer(
-                                    'assets/hotel.svg', 'Hotel', Colors.red),
-                                SizedBox(width: 10),
-                                buildIconContainer('assets/flight.svg',
-                                    'Flight', Colors.green),
-                                SizedBox(width: 10),
-                                buildIconContainer('assets/location.svg',
-                                    'Location', Colors.purple.shade300),
-                                SizedBox(width: 10),
-                                buildIconContainer('assets/food.svg', 'Food',
-                                    Colors.blueAccent.shade700),
-                              ],
-                            ),
+                            children: [
+                              buildHotelContainer(
+                                  'Emeralda Hotel',
+                                  'Paris, France',
+                                  '\$30',
+                                  'assets/hotel1.jpg',
+                                  4.9),
+                              SizedBox(width: 20),
+                              buildHotelContainer(
+                                  'Eco plus Hotel',
+                                  'Paris, France',
+                                  '\$40',
+                                  'assets/hotel2.jpeg',
+                                  4.6),
+                              SizedBox(width: 20),
+                              buildHotelContainer(
+                                  'Herald Hotel',
+                                  'Paris, France',
+                                  '\$30',
+                                  'assets/hotel3.jpeg',
+                                  4.9),
+                              SizedBox(width: 20),
+                            ],
                           ),
-                          SizedBox(height: 25),
-                          Container(
-                            padding: EdgeInsets.symmetric(horizontal: 16.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Popular Hotels',
-                                  style: TextStyle(
-                                    fontSize: 17,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                        ),
+                        SizedBox(height: 25),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Recently Booked',
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                                Text(
-                                  'See All',
-                                  style: TextStyle(
-                                      color: Colors.pink, fontSize: 13),
-                                ),
-                              ],
-                            ),
+                              ),
+                              Text(
+                                'See All',
+                                style: TextStyle(
+                                    color: Colors.pink, fontSize: 13),
+                              ),
+                            ],
                           ),
-                          SizedBox(height: 20),
-                          Container(
-                            height: 190,
-                            child: ListView(
-                              scrollDirection: Axis.horizontal,
-                              children: [
-                                buildHotelContainer(
-                                    'Emeralda Hotel',
-                                    'Paris, France',
-                                    '\$30',
-                                    'assets/hotel1.jpg',
-                                    4.9),
-                                SizedBox(width: 20),
-                                buildHotelContainer(
-                                    'Eco plus Hotel',
-                                    'Paris, France',
-                                    '\$40',
-                                    'assets/hotel2.jpeg',
-                                    4.6),
-                                SizedBox(width: 20),
-                                buildHotelContainer(
-                                    'Herald Hotel',
-                                    'Paris, France',
-                                    '\$30',
-                                    'assets/hotel3.jpeg',
-                                    4.9),
-                                SizedBox(width: 20),
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 25),
-                          Container(
-                            padding: EdgeInsets.symmetric(horizontal: 16.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Recently Booked',
-                                  style: TextStyle(
-                                    fontSize: 17,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  'See All',
-                                  style: TextStyle(
-                                      color: Colors.pink, fontSize: 13),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          Booked(),
-                        ],
-                      ),
+                        ),
+                        SizedBox(height: 10),
+                        Booked(),
+                      ],
                     ),
                   ),
                 ),
               ),
             ),
+          ),
           bottomNavigationBar: bottomNavBar(),
-              )
+        )
     );
   }
 }
